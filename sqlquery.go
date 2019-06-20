59ae557fe3e292f2d0e645e3f22ec7448e062708
CharLemAznable <545541819@qq.com>
2019-06-20 18:29:52 +0800

    Refactor required modules.

--- sqlquery.go
+++ sqlquery.go
@@ -10,10 +10,10 @@ import (
 	"fmt"
 	"github.com/bingoohuang/gou"
 
-	_ "github.com/go-goracle/goracle"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+	_ "gopkg.in/goracle.v2"
 )
 
 func selectDb(tid string) (string, string, string, error) {
